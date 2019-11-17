65d35dd67637ffd704861a095ae50b10f3f63dd6
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-17 15:13:12 +0530

    feat: (rpi) start a mosca server

---
+++ packages/rpi/broker.js
@@ -0,0 +1,12 @@
+const mosca = require('mosca');
+
+const moscaSettings = {
+    port: 1883, // PORT to run mosca on
+};
+
+// Start mosca
+const server = new mosca.Server(moscaSettings);
+
+server.on('ready', () => {
+    console.log(`> MQTT server running on http://localhost:${moscaSettings.port}`); // eslint-disable-line no-console
+});
