58caabc089ccaba591b11531e5c7d67398af8799
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-19 07:56:42 +0530

    chore: (rpi) show client connected status from broker

--- packages/rpi/broker.js
+++ packages/rpi/broker.js
@@ -7,6 +7,15 @@ const moscaSettings = {
 // Start mosca
 const server = new mosca.Server(moscaSettings);
 
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id); // eslint-disable-line no-console
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+    console.log('Published', packet.payload); // eslint-disable-line no-console
+});
+
 server.on('ready', () => {
     console.log(`> MQTT server running on mqtt://localhost:${moscaSettings.port}`); // eslint-disable-line no-console
 });
