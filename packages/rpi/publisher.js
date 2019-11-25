9fdca0b8644eeb7f865d702c002aa68300f37aba
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-25 20:27:57 +0530

    refactor: (rpi) add other listeners

--- packages/rpi/publisher.js
+++ packages/rpi/publisher.js
@@ -5,10 +5,48 @@ const serverIP = process.env.MQTT_SERVER_IP;
 // Connect to mqtt server
 const mqttPublisher = mqtt.connect(serverIP);
 
+/**
+ *  Emitted on successful (re)connection (i.e. connack rc=0).
+ */
 mqttPublisher.on('connect', function() {
     setInterval(function() {
         mqttPublisher.publish('sensors', 'Hello from mqtt publishers');
 
         console.log('Message Sent'); // eslint-disable-line no-console
     }, 5000);
+});
+
+/**
+ *  Emitted when a reconnect starts.
+ */
+mqttPublisher.on('reconnect', function() {
+    console.log('Reconnecting...'); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted when the client cannot connect (i.e. connack rc != 0) or when a parsing error occurs.
+ */
+mqttPublisher.on('error', function(err) {
+    console.log(err.message); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted after a disconnection.
+ */
+mqttPublisher.on('close', function(err) {
+    console.log('Disconnected'); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted after receiving disconnect packet from broker.
+ */
+mqttPublisher.on('disconnect', function(err) {
+    console.log('Broker sent disconnect packet'); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted when the client goes offline.
+ */
+mqttPublisher.on('offline', function(err) {
+    console.log('Client is now offline'); // eslint-disable-line no-console
 });
--- packages/rpi/subscriber.js
+++ packages/rpi/subscriber.js
@@ -3,16 +3,57 @@ const mqtt = require('mqtt');
 const serverIP = process.env.MQTT_SERVER_IP;
 
 // Connect to mqtt server
-const mqttClient = mqtt.connect(serverIP, { clientId: 'OpenAir' });
+const mqttSubscriber = mqtt.connect(serverIP);
 
-mqttClient.on('connect', function() {
-    mqttClient.subscribe('sensors');
+/**
+ *  Emitted on successful (re)connection (i.e. connack rc=0).
+ */
+mqttSubscriber.on('connect', function() {
+    mqttSubscriber.subscribe('sensors');
 
     console.log('subscribed'); // eslint-disable-line no-console
 });
 
-mqttClient.on('message', function(topic, message) {
+/**
+ *  Emitted when the client receives a publish packet
+ */
+mqttSubscriber.on('message', function(topic, message) {
     const context = message.toString();
 
     console.log(context); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted when a reconnect starts.
+ */
+mqttSubscriber.on('reconnect', function() {
+    console.log('Reconnecting...'); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted when the client cannot connect (i.e. connack rc != 0) or when a parsing error occurs.
+ */
+mqttSubscriber.on('error', function(err) {
+    console.log(err.message); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted after a disconnection.
+ */
+mqttSubscriber.on('close', function(err) {
+    console.log('Disconnected'); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted after receiving disconnect packet from broker.
+ */
+mqttSubscriber.on('disconnect', function(err) {
+    console.log('Broker sent disconnect packet'); // eslint-disable-line no-console
+});
+
+/**
+ *  Emitted when the client goes offline.
+ */
+mqttSubscriber.on('offline', function(err) {
+    console.log('Client is now offline'); // eslint-disable-line no-console
 });
