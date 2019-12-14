194b81d4605403f99b34cdc50c1d90d0081ffaf7
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-14 23:24:26 +0530

    fix: (server) set up sqlalchemy & flask-migrate

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     @app.route('/')
     def hello_world():
