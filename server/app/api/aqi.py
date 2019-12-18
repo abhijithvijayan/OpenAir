764d3f60722709262cbefa01b2d23aca56519b43
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-18 10:21:21 +0530

    fix: (server) get GOOGLE_MAPS_API_KEY from app.config in blueprint

--- server/app/api/aqi.py
+++ server/app/api/aqi.py
@@ -1,24 +1,20 @@
 import json
 import requests
-from flask import request
+from flask import request, current_app as flask_app
 
-# from manage import flask_app
 from app.api import bp
 
 
 @bp.route('/api/v1/getRoutesData', methods=['POST'])
 def getRoutesAQI():
     data = request.get_json()
-    print(data)
 
     start_location = data["start_location"]
     end_location = data["end_location"]
-    # GOOGLE_MAPS_API_KEY = flask_app.config['GOOGLE_MAPS_API_KEY']
-    GOOGLE_MAPS_API_KEY = ''
-    print(GOOGLE_MAPS_API_KEY)
+    GOOGLE_MAPS_API_KEY = flask_app.config["GOOGLE_MAPS_API_KEY"]
 
     r = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin={},{}&destination={},{}&sensor=false&units=metric&alternatives=true&key={}'.format(
-        start_location['latitude'], start_location['longitude'], end_location['latitude'], end_location['longitude'],
+        start_location["latitude"], start_location["longitude"], end_location["latitude"], end_location["longitude"],
         GOOGLE_MAPS_API_KEY))
 
     return json.loads(r.content.decode('utf-8-sig'))
