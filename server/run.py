928b0994c117a669a4ba422212ad46bc6dc50190
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-18 08:47:58 +0530

    fix: (server) create a shell context & add models to shell session

--- server/run.py
+++ server/run.py
@@ -1,6 +1,16 @@
-from app import create_app
+from app import create_app, db
+from app.models import Location
 
 app = create_app()
+
+
+"""
+Creates a shell context that adds the database instance and models to the shell session
+"""
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Location': Location}
+
 
 if __name__ == '__main__':
     app.run(host=app.config["FLASK_RUN_HOST"],
