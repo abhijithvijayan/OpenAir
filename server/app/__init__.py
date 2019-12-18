ba5e073ba3a80829b8c102f3e422a409090aa54a
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-18 09:18:43 +0530

    refactor: (server) use flask_app instead of app to resolve confusions

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,23 +1,24 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    flask_app = Flask(__name__)
+    flask_app.config.from_object(Config)
 
-    db.init_app(app)
-    migrate.init_app(app, db)
+    db.init_app(flask_app)
+    migrate.init_app(flask_app, db)
 
     from app.api import bp as api_bp
-    app.register_blueprint(api_bp)
+    flask_app.register_blueprint(api_bp)
 
-    return app
+    return flask_app
 
 
 from app import models
--- server/run.py
+++ server/run.py
@@ -1,17 +1,16 @@
 from app import create_app, db
 from app.models import Location
 
-app = create_app()
-
+flask_app = create_app()
 
 """
 Creates a shell context that adds the database instance and models to the shell session
 """
-@app.shell_context_processor
+@flask_app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Location': Location}
 
 
 if __name__ == '__main__':
-    app.run(host=app.config["FLASK_RUN_HOST"],
-            debug=app.config["FLASK_DEBUG"], port=app.config["FLASK_RUN_PORT"])
+    flask_app.run(host=flask_app.config["FLASK_RUN_HOST"],
+                  debug=flask_app.config["FLASK_DEBUG"], port=flask_app.config["FLASK_RUN_PORT"])
