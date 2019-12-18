e9eea2dda0c52413c1fe1ff54d379a98191f7851
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-18 09:47:18 +0530

    refactor: (server) separate logic for creating app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,24 +1,43 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from app.extensions import db, migrate
 
+from app import models
 from config import Config
-
-db = SQLAlchemy()
-migrate = Migrate()
 
 
 def create_app():
     flask_app = Flask(__name__)
     flask_app.config.from_object(Config)
 
-    db.init_app(flask_app)
-    migrate.init_app(flask_app, db)
-
-    from app.api import bp as api_bp
-    flask_app.register_blueprint(api_bp)
+    register_extensions(flask_app)
+    register_blueprints(flask_app)
+    register_shellcontext(flask_app)
 
     return flask_app
 
 
-from app import models
+def register_extensions(flask_app):
+    """Register Flask extensions."""
+    db.init_app(flask_app)
+    migrate.init_app(flask_app, db)
+
+
+def register_blueprints(flask_app):
+    """Register Flask blueprints."""
+    from app.api import bp as api_bp
+    flask_app.register_blueprint(api_bp)
+
+
+def register_shellcontext(flask_app):
+    """Register shell context objects.
+       Creates a shell context that adds the database
+       instance and models to the shell session
+    """
+    def make_shell_context():
+        """Shell context objects."""
+        return {
+            'db': db,
+            'Location': models.Location
+        }
+
+    flask_app.shell_context_processor(make_shell_context)
--- server/run.py
+++ server/run.py
@@ -1,15 +1,6 @@
-from app import create_app, db
-from app.models import Location
+from app import create_app
 
 flask_app = create_app()
-
-"""
-Creates a shell context that adds the database instance and models to the shell session
-"""
-@flask_app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Location': Location}
-
 
 if __name__ == '__main__':
     flask_app.run(host=flask_app.config["FLASK_RUN_HOST"],
