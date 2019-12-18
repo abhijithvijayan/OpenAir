cc454183db5c48ff1617d0e2bf1c23a56016edeb
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-18 17:39:25 +0530

    fix: (server) get points for multiple routes

--- server/app/api/aqi.py
+++ server/app/api/aqi.py
@@ -16,6 +16,37 @@ def distance_between_two_points(distance, start, destination):
     return dist
 
 
+def select_next_min_distant_point(distances, selected_step_pos):
+    # Iterate through distances[] and return next position
+    # with 3000m distance in between
+    all_visited = None
+    cur_selected = selected_step_pos
+    dist_from_selected = 0
+
+    if (selected_step_pos >= len(distances) - 1):
+        all_visited = True
+
+    for pos in range(selected_step_pos, len(distances)):
+        print('pos', pos)
+
+        dist_from_selected = distance_between_two_points(
+            distances, selected_step_pos, pos + 1)
+
+        if (dist_from_selected >= 3000):
+            selected_step_pos = pos
+            print('selected', selected_step_pos)
+            break
+
+        if (pos == len(distances) - 1):
+            all_visited = True
+
+    return {
+        'all_visited': all_visited,
+        'dist_from_selected': dist_from_selected,
+        'next_distant_point_pos': selected_step_pos
+    }
+
+
 @bp.route('/api/v1/getRoutesData', methods=['POST'])
 def getRoutesAQI():
     data = request.get_json()
@@ -27,40 +58,79 @@ def getRoutesAQI():
     # r = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin={},{}&destination={},{}&sensor=false&units=metric&alternatives=true&key={}'.format(
     #     start_location["latitude"], start_location["longitude"], end_location["latitude"], end_location["longitude"],
     #     GOOGLE_MAPS_API_KEY))
+    # map_data = json.loads(r.content.decode('utf-8-sig'))
 
+    # ToDo: Replace this with API Call in production
     with open('sample.json') as sample_file:
         map_data = json.load(sample_file)
-        routes = map_data["routes"]
+        map_routes = map_data["routes"]
 
-        distances = []
-        selected_step = None
-        nearest_aqi_node = None
-        dist_from_selected = 0
+        routes = []
 
-        if routes is not None and len(routes) > 0:
-            current_route = routes[0]
-            if current_route["legs"] is not None:
-                # ToDo: There can be multiple legs
-                for leg in current_route["legs"]:
-                    if leg["steps"] is not None:
-                        # iterate through each step
-                        for step in leg["steps"]:
-                            # store all distance between each point
-                            distances.append(step["distance"]["value"])
+        if map_routes is not None and len(map_routes) > 0:
+            for current_route in map_routes:
+                legs = []
+                if current_route["legs"] is not None:
+                    # ToDo: There can be multiple legs
+                    for leg in current_route["legs"]:
+                        if leg["steps"] is not None:
+                            distances = []
+                            nearest_aqi_points = []
+                            next_distant_point_pos = 0
 
-                        # Find the next step that has minimum of 3000m distance in between
-                        selected_step_pos = 0
-                        for pos in range(len(distances)):
-                            dist_from_selected = distance_between_two_points(
-                                distances, selected_step_pos, pos + 1)
-                            if (dist_from_selected >= 3000):
-                                selected_step_pos = pos
-                                break
+                            # iterate through each step
+                            for step in leg["steps"]:
+                                # store all distance between each point
+                                distances.append(step["distance"]["value"])
 
-                        selected_step = leg["steps"][selected_step_pos]
-                        # Find nearest values within 1500m of this coordinates from database
-                        nearest_aqi_node = Location.get_nearby_aqi_node(
-                            selected_step["start_location"]["lat"], selected_step["start_location"]["lng"])
+                            print('distances', distances)
 
-        # return json.loads(r.content.decode('utf-8-sig'))
-    return jsonify({'selected': selected_step["start_location"], 'nearest': nearest_aqi_node})
+                            for i in range(len(distances)):
+                                # Find the next step that has minimum of 3000m distance in between
+                                selected_response = select_next_min_distant_point(
+                                    distances, next_distant_point_pos)
+
+                                print(selected_response)
+
+                                if (selected_response["all_visited"]):
+                                    break
+
+                                # Found a 3000m distant location
+                                next_distant_point_pos = selected_response["next_distant_point_pos"]
+
+                                # Get selected leg from original data
+                                print(next_distant_point_pos)
+                                selected_step = leg["steps"][next_distant_point_pos]
+                                # Find nearest values within 1500m of this coordinates from database
+                                nearest_aqi_node = Location.get_nearby_aqi_node(
+                                    selected_step["start_location"]["lat"], selected_step["start_location"]["lng"])
+
+                                # Leg to draw on map with aqi values of 1500m near node
+                                new_point_on_map = {
+                                    'aqi': nearest_aqi_node["aqi"],
+                                    'updated_at': nearest_aqi_node["updated_at"],
+                                    'start_location': selected_step["start_location"],
+                                    'end_location': selected_step["end_location"],
+                                    'distance': {
+                                        'value': selected_response["dist_from_selected"],
+                                    },
+                                    'polyline': selected_step["polyline"],
+                                }
+                                # Push to array of aqi points
+                                nearest_aqi_points.append(new_point_on_map)
+
+                                # move to next position & continue iteration
+                                next_distant_point_pos += 1
+
+                            legs.append({
+                                'steps': nearest_aqi_points
+                            })
+
+                    routes.append({
+                        'legs': legs
+                    })
+
+    return jsonify({
+        'routes': routes,
+        'status': 'ok'
+    })
