232453f1dfad07d0aa0cac1bcfb84199397fc752
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-27 17:20:18 +0530

    feat: (lib) create a arduino library for sensors

---
+++ lib/OpenAirSensors/src/OpenAirSensors.h
@@ -0,0 +1,14 @@
+/*
+  OpenAirSensors.h - Library for OpenAir Project.
+  Created by Abhijith Vijayan, Decemeber 26, 2019.
+*/
+
+#include "Arduino.h"
+
+#ifndef OpenAirSensors_h
+#define OpenAirSensors_h
+
+#include "core/Sensors.h"
+#include "core/SensorCollection.h"
+
+#endif
