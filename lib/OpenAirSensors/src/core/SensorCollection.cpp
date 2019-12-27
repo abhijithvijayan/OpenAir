3868fcfe98f98993d0b6fa7fceb9e293abdc406e
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-27 18:55:01 +0530

    chore: (lib) store start & end channel# of mux to collection

--- lib/OpenAirSensors/src/core/SensorCollection.cpp
+++ lib/OpenAirSensors/src/core/SensorCollection.cpp
@@ -1,10 +1,12 @@
 #include "OpenAirSensors.h"
 
-SensorCollection::SensorCollection(char *id, char *name)
+SensorCollection::SensorCollection(char *id, char *name, int ch_start, int ch_end)
 {
   _id = id,
   _name = name;
   _size = 0;
+  _ch_start = ch_start;
+  _ch_end = ch_end;
 }
 
 void SensorCollection::setup()
@@ -29,6 +31,16 @@ char *SensorCollection::getName() const
 int SensorCollection::getSize() const
 {
   return _size;
+}
+
+int SensorCollection::getStartChannel() const
+{
+  return _ch_start;
+}
+
+int SensorCollection::getEndChannel() const
+{
+  return _ch_end;
 }
 
 void SensorCollection::addSensor(Sensor &sensor)
--- lib/OpenAirSensors/src/core/SensorCollection.h
+++ lib/OpenAirSensors/src/core/SensorCollection.h
@@ -3,12 +3,14 @@ const int MAX_SENSORS = 8;
 class SensorCollection
 {
 public:
-  SensorCollection(char *id, char *name);
+  SensorCollection(char *id, char *name, int ch_start, int ch_end);
   void setup();
 
   char *getId() const;
   char *getName() const;
   int getSize() const;
+  int getStartChannel() const;
+  int getEndChannel() const;
   void addSensor(Sensor &sensor);
 
   Sensor *getSensor(int index);
@@ -19,4 +21,6 @@ private:
   int _size;
   char *_id;
   char *_name;
+  int _ch_start;
+  int _ch_end;
 };
