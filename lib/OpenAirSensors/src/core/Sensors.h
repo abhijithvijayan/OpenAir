a00e25650f359173a5a66738af5526c424ec591c
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-27 20:47:45 +0530

    refactor: (lib) minor tweaks to sensor class

--- lib/OpenAirSensors/src/core/Sensors.h
+++ lib/OpenAirSensors/src/core/Sensors.h
@@ -1,36 +1,43 @@
 class Sensor
 {
 public:
-  Sensor(char *id, char *name, char *type);
+  Sensor(char *id, char *name, char *type, int pin);
   virtual void setup();
   virtual float read();
 
   char *getId() const;
   char *getName() const;
   char *getType() const;
+  int getPin() const;
 
 protected:
   char *_id;
   char *_name;
   char *_type;
+  int _pin;
 };
 
 class AnalogSensor : public Sensor
 {
 public:
-  virtual void setup();
+  AnalogSensor(char *id, char *name, char *type, int pin);
+
   virtual float read();
+
+protected:
+  bool _isAnalog;
 };
 
 class Mux
 {
 public:
-  Mux(int selector_pin_1, int selector_pin_2, int selector_pin_3);
+  Mux(int selector_pin_0, int selector_pin_1, int selector_pin_2);
 
   virtual void setup();
+  virtual void switchChannel(int channel);
 
-private:
+protected:
+  int _selector_pin_0;
   int _selector_pin_1;
   int _selector_pin_2;
-  int _selector_pin_3;
-}
+};
