8a443986d64c830792268e036a4d014a4b9b075b
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-12-29 22:53:53 +0530

    feat: (lib) add getVoltage() method for mq sensor

--- lib/OpenAirSensors/src/core/MQSensors.cpp
+++ lib/OpenAirSensors/src/core/MQSensors.cpp
@@ -1,7 +1,25 @@
 #include "Arduino.h"
 #include "OpenAirSensors.h"
 
+#define retries 5
+#define retry_interval 20
+
 MQSensor::MQSensor(char *id, char *name, char *category, int pin, int type) : AnalogSensor(id, name, category, pin)
 {
   this->_type = type;
+}
+
+double MQSensor::getVoltage()
+{
+  double avg = 0.0, voltage;
+
+  for (int i = 0; i < retries; ++i)
+  {
+    avg += this->read();
+    delay(retry_interval);
+  }
+
+  voltage = (avg / retries) * _VOLT_RESOLUTION / (pow(2, ADC_RESOLUTION) - 1);
+
+  return voltage;
 }
--- lib/OpenAirSensors/src/core/MQSensors.h
+++ lib/OpenAirSensors/src/core/MQSensors.h
@@ -1,8 +1,14 @@
+#define ADC_RESOLUTION 10 // for 10bit analog to digital converter (eg: NodeMCU).
+
 class MQSensor : public AnalogSensor
 {
 public:
   MQSensor(char *id, char *name, char *category, int pin, int type);
 
+  double getVoltage();
+
 protected:
   int _type;
+  float _sensor_voltage;
+  float _VOLT_RESOLUTION = 5.0; // if 3.3v use 3.3
 };
