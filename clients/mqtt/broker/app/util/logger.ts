ed1c5de7eadcbd38a7aeb7122924d16d955648bc
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-01-14 18:50:52 +0530

    refactor: define ts definitions for logger

--- clients/mqtt/broker/app/util/logger.ts
+++ clients/mqtt/broker/app/util/logger.ts
@@ -1,23 +1,26 @@
-const { createLogger, format, transports } = require('winston');
+import { createLogger, format, transports, Logger, LoggerOptions } from 'winston';
 
-const logLevel = () => {
+const logLevel = (): string => {
     if (process.env.NODE_ENV === 'production') {
         return 'info';
     }
+
     return 'debug';
 };
 
-const logger = createLogger({
+const loggerOptions: LoggerOptions = {
     format: format.combine(
         // Use these two instead for JSON format
         // format.timestamp(),
         // format.json()
         format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss.SSS' }),
-        format.printf(info => {
+        format.printf((info): string => {
             return `[${info.timestamp}] [${info.level.toUpperCase()}] ${info.message}`;
         })
     ),
     transports: [new transports.Console({ level: logLevel() })],
-});
+};
 
-module.exports = logger;
+const logger: Logger = createLogger(loggerOptions);
+
+export default logger;
