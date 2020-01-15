f962378391406f1da911e936b5f6d3a9d51d12a5
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-01-15 16:26:00 +0530

    refactor: (subscriber) mqtt callbacks : rewrite ts

--- clients/mqtt/subscriber/app/subscriber.ts
+++ clients/mqtt/subscriber/app/subscriber.ts
@@ -1,34 +1,33 @@
-const mqtt = require('mqtt');
+import * as mqtt from 'mqtt';
 
-const { MQTT_SERVER_ADDRESS, MQTT_AUTH_ID, MQTT_AUTH_PASSWORD } = require('./config/secrets');
+import { MQTT_SERVER_ADDRESS, MQTT_AUTH_ID, MQTT_AUTH_PASSWORD } from './config/secrets';
 
-const startSubscriber = () => {
-    const subscriberId = `SUBSCRIBER_${Math.random()
+const startSubscriber = (): void => {
+    const subscriberId: string = `SUBSCRIBER_${Math.random()
         .toString(16)
         .substr(2, 8)}`;
 
     /**
      *  Connection Options
      */
-    const mqttOptions = {
+    const mqttOptions: mqtt.IClientOptions = {
         clientId: subscriberId,
         username: MQTT_AUTH_ID,
         password: MQTT_AUTH_PASSWORD,
         keepalive: 60,
         reconnectPeriod: 1000,
-        encoding: 'utf8',
         clean: true,
     };
 
     /**
      *  Connect to mqtt server
      */
-    const mqttClient = mqtt.connect(MQTT_SERVER_ADDRESS, mqttOptions);
+    const mqttClient: mqtt.MqttClient = mqtt.connect(MQTT_SERVER_ADDRESS, mqttOptions);
 
     /**
      *  Emitted on successful mqtt (re)connection
      */
-    mqttClient.on('connect', () => {
+    mqttClient.on('connect', (): void => {
         // Subscribe to all topics
         mqttClient.subscribe('sensors');
 
@@ -38,8 +37,8 @@ const startSubscriber = () => {
     /**
      *  Emitted when the mqtt client receives a published packet
      */
-    mqttClient.on('message', (topic, message) => {
-        const context = message.toString();
+    mqttClient.on('message', (_topic, message): void => {
+        const context: string = message.toString();
 
         console.log(context); // eslint-disable-line no-console
 
@@ -52,37 +51,40 @@ const startSubscriber = () => {
     /**
      *  Emitted when a mqtt reconnect starts.
      */
-    mqttClient.on('reconnect', function() {
+    mqttClient.on('reconnect', (): void => {
         console.log('Reconnecting...'); // eslint-disable-line no-console
     });
 
     /**
      *  Emitted when mqtt client cannot connect or when a parsing error occurs.
      */
-    mqttClient.on('error', function(err) {
-        console.log(err.message); // eslint-disable-line no-console
+    mqttClient.on('error', (err: mqtt.OnErrorCallback): void => {
+        console.log(err); // eslint-disable-line no-console
     });
 
     /**
      *  Emitted after a mqtt disconnection.
      */
-    mqttClient.on('close', function(err) {
+    mqttClient.on('close', (err: mqtt.OnErrorCallback): void => {
         console.log('Disconnected'); // eslint-disable-line no-console
+        console.log(err);
     });
 
     /**
      *  Emitted after receiving disconnect packet from mqtt broker.
      */
-    mqttClient.on('disconnect', function(err) {
+    mqttClient.on('disconnect', (err: mqtt.OnErrorCallback): void => {
         console.log('Broker sent disconnect packet'); // eslint-disable-line no-console
+        console.log(err);
     });
 
     /**
      *  Emitted when the mqtt client goes offline.
      */
-    mqttClient.on('offline', function(err) {
+    mqttClient.on('offline', (err: mqtt.OnErrorCallback): void => {
         console.log('Client is now offline'); // eslint-disable-line no-console
+        console.log(err);
     });
 };
 
-module.exports = startSubscriber;
+export default startSubscriber;
