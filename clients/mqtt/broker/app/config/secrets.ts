16c61aa6b9d821e09af0917498b9fce9f68d52c3
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-01-15 12:36:19 +0530

    refactor: (broker) secrets: rewrite ts

---
+++ clients/mqtt/broker/app/config/secrets.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+import logger from '../util/logger';
+
+if (!fs.existsSync('.env')) {
+    logger.info('No .env file found, looking for variables in environment.');
+} else {
+    // Load environment variables from .env file
+    dotenv.config({ path: '.env' });
+}
+
+// typings for env vars
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production';
+            SOCKET_SERVER_PORT: number;
+            MQTT_AUTH_ID: string;
+            MQTT_AUTH_PASSWORD: string;
+        }
+    }
+}
+
+/**
+ *  Export all the env vars here
+ */
+export const PRODUCTION: string = 'production';
+export const ENVIRONMENT: string = process.env.NODE_ENV || 'development';
+export const { SOCKET_SERVER_PORT = 8888 } = process.env;
+export const { MQTT_AUTH_ID } = process.env;
+export const { MQTT_AUTH_PASSWORD } = process.env;
+
+/**
+ *  Array of required fields
+ */
+const requiredSecrets: string[] = ['MQTT_AUTH_ID', 'MQTT_AUTH_PASSWORD'];
+
+for (const secret of requiredSecrets) {
+    if (!process.env[secret]) {
+        logger.error(`Env variable ${secret} is missing.`);
+        process.exit(1);
+    }
+}
