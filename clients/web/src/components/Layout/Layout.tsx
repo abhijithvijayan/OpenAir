778b4d09bd522e5432e72061e5e6f0518c2dba78
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-01-20 10:18:28 +0530

    refactor: (web) ts rewrite Layout component

--- clients/web/src/components/Layout/Layout.tsx
+++ clients/web/src/components/Layout/Layout.tsx
@@ -3,17 +3,22 @@ import styled from 'styled-components';
 
 import Loader from '../Loader';
 
+type WrapperProperties = {
+    children: React.ReactNode;
+};
+
 const BodyWrapper = styled.main`
     position: relative;
     min-height: 100vh;
+
     .content__wrapper {
         min-height: 100vh;
         width: 100%;
     }
 `;
 
-const Wrapper = ({ children }) => {
-    // Get from props
+const Wrapper = ({ children }: WrapperProperties): JSX.Element => {
+    // ToDo: Get from props
     const isPageLoading = false;
     const renderContent = isPageLoading ? <Loader /> : children;
 
