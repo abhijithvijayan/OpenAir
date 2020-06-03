c5dbc833fd41ff95f1c8e454e75e86d37439f8e6
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-06-03 20:49:35 +0530

    feat(web): use tailwindcss(with twin.macro)

---
+++ clients/web/babel-plugin-macros.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  twin: {
+    preset: 'styled-components',
+    config: 'tailwind.config.js',
+    hasSuggestions: true,
+    debug: false,
+    autoCssProp: true, // This adds the css prop when it's needed
+  },
+};
