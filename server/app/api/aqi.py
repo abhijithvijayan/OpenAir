bfce0057b790b76a56b7380e9ada3a23f67ba0a6
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-06-15 01:57:50 +0530

    refactor(server): for now use sample json file to get data

--- server/app/api/aqi.py
+++ server/app/api/aqi.py
@@ -2,6 +2,7 @@ from flask import request, current_app as flask_app
 import googlemaps
 import json
 
+from app.api.errors import badrequest
 from app.models import Place
 from app.api import bp
 
@@ -58,105 +59,107 @@ def getRoutesAQI():
     end_location = data["end_location"]
 
     # get directions
-    map_routes = gmaps.directions('{},{}'.format(start_location["lat"], start_location["lng"]), '{},{}'.format(end_location["lat"], end_location["lng"]),
-                                  mode="driving", units="metric", alternatives=True)
+    # map_routes = gmaps.directions('{},{}'.format(start_location["lat"], start_location["lng"]), '{},{}'.format(end_location["lat"], end_location["lng"]),
+    #                               mode="driving", units="metric", alternatives=True)
 
     # ToDo: for development only
-    # with open('sample.json') as sample_file:
-    #     map_data = json.load(sample_file)
-    #     map_routes = map_data["routes"]
+    with open('sample.json') as sample_file:
+        map_data = json.load(sample_file)
+        map_routes = map_data["routes"]
 
-    routes = []
+        routes = []
 
-    if map_routes is not None and len(map_routes) > 0:
-        for current_route in map_routes:
-            legs = []
-            if current_route["legs"] is not None:
-                for leg in current_route["legs"]:
-                    if leg["steps"] is not None:
-                        distances = []
-                        nearest_aqi_points = []
-                        next_distant_point_pos = 0
+        if map_routes is not None and len(map_routes) > 0:
+            for current_route in map_routes:
+                legs = []
+                if current_route["legs"] is not None:
+                    for leg in current_route["legs"]:
+                        if leg["steps"] is not None:
+                            distances = []
+                            nearest_aqi_points = []
+                            next_distant_point_pos = 0
 
-                        # iterate through each step
-                        # store distance between each point (except destination)
-                        for cur_step in range(len(leg["steps"]) - 1):
-                            step = leg["steps"][cur_step]
-                            distances.append(step["distance"]["value"])
+                            # iterate through each step
+                            # store distance between each point (except destination)
+                            for cur_step in range(len(leg["steps"]) - 1):
+                                step = leg["steps"][cur_step]
+                                distances.append(step["distance"]["value"])
 
-                        # Manually find nearby points of source & destination of route
-                        for pos in [0, -1]:
-                            static_point = leg["steps"][pos]
-                            lat = static_point["start_location" if pos ==
-                                               0 else "end_location"]["lat"]
-                            lng = static_point["start_location" if pos ==
-                                               0 else "end_location"]["lng"]
-                            # Find nearest node_aqi from the point
-                            nearest_aqi_node = Place.get_nearby_aqi_node(
-                                lat, lng)
-                            point_on_map = {
-                                'aqi': nearest_aqi_node["aqi"] if (nearest_aqi_node is not None) else None,
-                                'updated_at': nearest_aqi_node["updated_at"] if (nearest_aqi_node is not None) else None,
-                                'location': static_point["start_location" if pos ==
-                                                         0 else "end_location"],
-                                'distance':
-                                    {'value': 0} if pos == 0 else leg["distance"],
-                                'polyline': {},
-                            }
-                            # Push point to array of aqi points
-                            nearest_aqi_points.append(point_on_map)
-
-                        # Find the next subsequent points(excluding destination)
-                        for _ in range(len(distances)):
-                            # Find the next step that has minimum threshold distance in between
-                            selected_response = select_next_min_distant_point(
-                                distances, next_distant_point_pos)
-
-                            # Exit if all visited
-                            if (selected_response["all_visited"] and selected_response["is_selected"] is None):
-                                break
-
-                            # threshold distant location Found!
-                            next_distant_point_pos = selected_response["next_distant_point_pos"]
-                            # Get selected leg from original data
-                            selected_step = leg["steps"][next_distant_point_pos]
-                            # Find nearest values within 1500m of this coordinate from database
-                            nearest_aqi_node = Place.get_nearby_aqi_node(
-                                selected_step["end_location"]["lat"], selected_step["end_location"]["lng"])
-
-                            # Push value if aqi_node found
-                            if (nearest_aqi_node is not None):
-                                # Leg to draw on map with aqi values of 1500m near node
-                                new_point_on_map = {
+                            # Manually find nearby points of source & destination of route
+                            for pos in [0, -1]:
+                                static_point = leg["steps"][pos]
+                                lat = static_point["start_location" if pos ==
+                                                   0 else "end_location"]["lat"]
+                                lng = static_point["start_location" if pos ==
+                                                   0 else "end_location"]["lng"]
+                                # Find nearest node_aqi from the point
+                                nearest_aqi_node = Place.get_nearby_aqi_node(
+                                    lat, lng)
+                                point_on_map = {
                                     'aqi': nearest_aqi_node["aqi"] if (nearest_aqi_node is not None) else None,
                                     'updated_at': nearest_aqi_node["updated_at"] if (nearest_aqi_node is not None) else None,
-                                    'location': selected_step["end_location"],
-                                    'distance': {
-                                        'value': selected_response["dist_from_selected"],
-                                    },
-                                    'polyline': selected_step["polyline"],
+                                    'location': static_point["start_location" if pos ==
+                                                             0 else "end_location"],
+                                    'distance':
+                                        {'value': 0} if pos == 0 else leg["distance"],
+                                    'polyline': {},
                                 }
-                                # Push to array of aqi points (before the last entry)
-                                nearest_aqi_points.insert(
-                                    len(nearest_aqi_points) - 1, new_point_on_map)
+                                # Push point to array of aqi points
+                                nearest_aqi_points.append(point_on_map)
 
-                            # If last item was selected, stop iteration
-                            if (selected_response["all_visited"] and selected_response["is_selected"]):
-                                break
-                            else:
-                                # move to next position & continue iteration
-                                next_distant_point_pos += 1
+                            # Find the next subsequent points(excluding destination)
+                            for _ in range(len(distances)):
+                                # Find the next step that has minimum threshold distance in between
+                                selected_response = select_next_min_distant_point(
+                                    distances, next_distant_point_pos)
 
-                        legs.append({
-                            'steps': nearest_aqi_points
-                        })
+                                # Exit if all visited
+                                if (selected_response["all_visited"] and selected_response["is_selected"] is None):
+                                    break
 
-                routes.append({
-                    'legs': legs,
-                    'summary': current_route["summary"]
-                })
+                                # threshold distant location Found!
+                                next_distant_point_pos = selected_response["next_distant_point_pos"]
+                                # Get selected leg from original data
+                                selected_step = leg["steps"][next_distant_point_pos]
+                                # Find nearest values within 1500m of this coordinate from database
+                                nearest_aqi_node = Place.get_nearby_aqi_node(
+                                    selected_step["end_location"]["lat"], selected_step["end_location"]["lng"])
 
-    return {
-        'routes': routes,
-        'status': 'ok'
-    }
+                                # Push value if aqi_node found
+                                if (nearest_aqi_node is not None):
+                                    # Leg to draw on map with aqi values of 1500m near node
+                                    new_point_on_map = {
+                                        'aqi': nearest_aqi_node["aqi"] if (nearest_aqi_node is not None) else None,
+                                        'updated_at': nearest_aqi_node["updated_at"] if (nearest_aqi_node is not None) else None,
+                                        'location': selected_step["end_location"],
+                                        'distance': {
+                                            'value': selected_response["dist_from_selected"],
+                                        },
+                                        'polyline': selected_step["polyline"],
+                                    }
+                                    # Push to array of aqi points (before the last entry)
+                                    nearest_aqi_points.insert(
+                                        len(nearest_aqi_points) - 1, new_point_on_map)
+
+                                # If last item was selected, stop iteration
+                                if (selected_response["all_visited"] and selected_response["is_selected"]):
+                                    break
+                                else:
+                                    # move to next position & continue iteration
+                                    next_distant_point_pos += 1
+
+                            legs.append({
+                                'steps': nearest_aqi_points
+                            })
+
+                    routes.append({
+                        'legs': legs,
+                        'summary': current_route["summary"]
+                    })
+
+        return {
+            'routes': routes,
+            'status': 'ok'
+        }
+
+    return badrequest('Error opening the sample file')
